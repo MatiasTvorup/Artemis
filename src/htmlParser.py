cc22bfd2bb0893eb26d4eebe4be8efaac5fa0277
matias <Matias@Tvorup.eu>
2024-02-04 17:41:33 +0100

    added beautiful soup

---
+++ src/htmlParser.py
@@ -0,0 +1,11 @@
+
+
+class HtmlParser:
+    __htmlContent__ = ""
+
+    def __init__(self, htmlContent : str):
+        self.__htmlContent__ = htmlContent
+        pass
+
+    def parse(self):
+        pass
